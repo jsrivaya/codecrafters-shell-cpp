cf78cdfeba0b4541dda9508fecc8df1986235254
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-05 12:53:24 -0800

    Remove dead code

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,6 @@
 
 #include "shell.hpp"
 
-std::vector<std::string> get_args (const std::string& command) {
-  std::istringstream iss(command);
-  std::vector<std::string> tokens;
-  std::string token;
-  while (iss >> token) {
-      tokens.emplace_back(token);
-  }
-  return tokens;
-}
-  
-
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
