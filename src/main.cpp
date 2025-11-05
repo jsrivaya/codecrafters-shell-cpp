9774917b0c7835182839397ed5e99d9a8faade72
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-05 08:01:36 -0800

    Add type command

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <unordered_set>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  std::unordered_set<std::string> commands{"exit", "echo", "type"};
 
   while(true) {
     std::cout << "$ ";
@@ -17,6 +19,9 @@ int main() {
       exit(0);
     } else if (command.starts_with("echo")) {
       std::cout << command.substr(5) << std::endl;
+    } else if (command.starts_with("type")) {
+      if(commands.find(command.substr(5)) != commands.end()) std::cout << command.substr(5) << "is a shell builtin" << std::endl;
+      else std::cout << command.substr(5) << ": is a shell builtin" << std::endl;
     } else {
       std::cout << command << ": command not found" << std::endl;
     }
