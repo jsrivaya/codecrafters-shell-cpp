ff9cd800c93c522e3388c62020da24e399e738bd
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-05 08:06:19 -0800

    Fix type command

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,8 @@ int main() {
     } else if (command.starts_with("echo")) {
       std::cout << command.substr(5) << std::endl;
     } else if (command.starts_with("type")) {
-      if(commands.find(command.substr(5)) != commands.end()) std::cout << command.substr(5) << "is a shell builtin" << std::endl;
-      else std::cout << command.substr(5) << ": is a shell builtin" << std::endl;
+      if(commands.find(command.substr(4)) != commands.end()) std::cout << command.substr(5) << ": not found" << std::endl;
+      else std::cout << command.substr(4) << " is a shell builtin" << std::endl;
     } else {
       std::cout << command << ": command not found" << std::endl;
     }
