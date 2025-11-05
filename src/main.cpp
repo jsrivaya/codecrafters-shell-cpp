e41a8d00dbf25a88b6660e9aab5621f7fa181fe8
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-05 06:03:46 -0800

    Add initial echo command

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -11,8 +13,16 @@ int main() {
     std::string command{};
     std::getline(std::cin, command);
 
-    if(command.starts_with("exit ")) {
+    if(command.starts_with("exit")) {
       exit(0);
+    } else if (command.starts_with("echo")) {
+      std::vector<std::string> tokens{};
+      std::istringstream iss(command);
+      std::string token;
+      while (iss >> token) {
+          tokens.push_back(token);
+          if(token != "echo") std::cout << token << std::endl;
+      }
     }
 
     std::cout << command << ": command not found" << std::endl;
