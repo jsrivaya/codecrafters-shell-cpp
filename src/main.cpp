1ce6d4896552f70882ba44b8aeb03a96de18de83
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-05 07:41:28 -0800

    Fix echo command

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,10 @@ int main() {
     if(command.starts_with("exit")) {
       exit(0);
     } else if (command.starts_with("echo")) {
-      std::vector<std::string> tokens{};
       std::istringstream iss(command);
-      std::string token;
+      std::string token{};
       while (iss >> token) {
-          tokens.push_back(token);
-          if(token != "echo") std::cout << token << std::endl;
+          if(token != "echo") std::cout << token << " ";
       }
     }
 
