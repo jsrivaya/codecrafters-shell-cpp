37a60711c8b81ff98d80bc1c1e9b7c967e0ceefa
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-05 08:08:20 -0800

    Fix space in builtin type

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ int main() {
       std::cout << command.substr(5) << std::endl;
     } else if (command.starts_with("type")) {
       if(commands.find(command.substr(4)) != commands.end()) std::cout << command.substr(5) << ": not found" << std::endl;
-      else std::cout << command.substr(4) << " is a shell builtin" << std::endl;
+      else std::cout << command.substr(4) << "is a shell builtin" << std::endl;
     } else {
       std::cout << command << ": command not found" << std::endl;
     }
