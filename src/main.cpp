182fcdc00d7b8103278cd8d94fc551587afe5e46
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-05 09:13:10 -0800

    Add initial version of find feature

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,45 @@
 #include <sstream>
 #include <vector>
 #include <unordered_set>
+#include <filesystem>
+
+#include <cstdlib>
+#include <unistd.h>
+
+std::string find(const std::string& command) {
+  
+  std::string path_env{};
+  if(const auto p = std::getenv("PATH"); p) path_env = p;
+  else return "";
+
+  size_t start = 0;
+  size_t end = path_env.find_first_of(":");
+  while(start < path_env.size()) {
+    std::string dir = path_env.substr(start, end - start);
+    std::filesystem::path p = dir;
+    p /= command;
+    if (std::filesystem::exists(p)) {
+      return p.string();
+    }
+  }
+
+  return "";
+}
+
+bool is_exec(const std::string& path) {
+  if(std::filesystem::exists(path)) return (access(path.c_str(), X_OK) == 0);
+  return false;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   std::unordered_set<std::string> commands{"exit", "echo", "type"};
+
+  auto is_builtin = [commands](const std::string& command) {
+    return commands.find(command.substr(5)) != commands.end();
+  };
 
   while(true) {
     std::cout << "$ ";
@@ -20,8 +53,11 @@ int main() {
     } else if (command.starts_with("echo")) {
       std::cout << command.substr(5) << std::endl;
     } else if (command.starts_with("type")) {
-      if(commands.find(command.substr(5)) == commands.end()) std::cout << command.substr(5) << ": not found" << std::endl;
-      else std::cout << command.substr(5) << " is a shell builtin" << std::endl;
+      if(is_builtin(command)) std::cout << command.substr(5) << " is a shell builtin" << std::endl;
+      else {
+        if (auto path = find(command); path != "" && is_exec(path)) std::cout << command.substr(5) << path << std::endl;
+        else std::cout << command.substr(5) << ": not found" << std::endl;
+      }
     } else {
       std::cout << command << ": command not found" << std::endl;
     }
