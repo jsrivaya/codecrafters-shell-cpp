43911e062f1da6bee36cf79055cfdc0b73127ce4
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-05 08:11:56 -0800

    Fix type condition

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main() {
     } else if (command.starts_with("echo")) {
       std::cout << command.substr(5) << std::endl;
     } else if (command.starts_with("type")) {
-      if(commands.find(command.substr(5)) != commands.end()) std::cout << command.substr(5) << ": not found" << std::endl;
+      if(commands.find(command.substr(5)) == commands.end()) std::cout << command.substr(5) << ": not found" << std::endl;
       else std::cout << command.substr(5) << " is a shell builtin" << std::endl;
     } else {
       std::cout << command << ": command not found" << std::endl;
