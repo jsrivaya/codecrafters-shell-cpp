7e5fa0b4d0589056c3aaee174f8baeec4da87560
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-06 07:12:44 -0800

    Add support for 'cd' command

--- src/command_builtin.hpp
+++ src/command_builtin.hpp
@@ -11,7 +11,8 @@ class BuiltinCommand : public Command {
     public:
         BuiltinCommand(const std::string& name) : Command(name, "builtin") { };
         void execute(const std::string& args = "") {
-            if (get_name() == "echo") exec_echo(args); 
+            if (get_name() == "cd") exec_cd(args); 
+            else if (get_name() == "echo") exec_echo(args); 
             else if (get_name() == "exit") exit(0);
             else if (get_name() == "pwd") exec_pwd();
             else if (get_name() == "type") exec_type(args);
@@ -20,10 +21,16 @@ class BuiltinCommand : public Command {
             return get_name();
         }
         static bool is_builtin(const std::string& name) {
-            return name == "echo" || name == "exit" || name == "pwd" || name == "type";
+            return name == "cd" || name == "echo" || name == "exit" || name == "pwd" || name == "type";
         }
 
     private:
+        void exec_cd(const std::string& path) {
+            if (std::filesystem::exists(std::filesystem::path(path)))
+                std::filesystem::current_path(path);
+            else
+                std::cout << "cd: " + path + ": No such file or directory" << std::endl;
+        }
         void exec_echo(const std::string& args) {
             std::cout << args << std::endl;
         }
