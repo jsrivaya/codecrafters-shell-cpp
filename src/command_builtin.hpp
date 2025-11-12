79d7cf462c6cab5f0f73c36534e7256cf3d4cd37
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-12 01:37:25 -0800

    Add too many arguments error state to cd command

--- src/command_builtin.hpp
+++ src/command_builtin.hpp
@@ -14,7 +14,7 @@ class BuiltinCommand : public Command {
         void execute(const  std::vector<std::string>&  args = {}) {
             // Builtin commands like 'cd' need to modify the shell's own state
             // others like: echo, exit, pwd, type are simple enough that forking would cause too much overhead
-            if (get_name() == "cd") cd(args.at(0)); 
+            if (get_name() == "cd") cd(args); 
             else if (get_name() == "echo") echo(args); 
             else if (get_name() == "exit") exit(0);
             else if (get_name() == "pwd") pwd();
@@ -28,13 +28,16 @@ class BuiltinCommand : public Command {
         }
 
     private:
-        void cd(const std::string& path) {
-            if (std::filesystem::exists(std::filesystem::path(path)))
-                std::filesystem::current_path(path);
-            else if (auto p = std::getenv("HOME"); path == "~" && p)
-                    std::filesystem::current_path(std::string{p});
+        void cd(const  std::vector<std::string>& path) {
+            if (auto p = std::getenv("HOME"); path.empty() ||
+                (path.size() == 1 && path.at(0) == "~" && p))
+                std::filesystem::current_path(std::string{p});
+            else if (path.size() > 1)
+                std::cerr << "cd: : too many arguments" << std::endl;
+            else if (std::filesystem::exists(std::filesystem::path(path.at(0))))
+                std::filesystem::current_path(path.at(0));
             else
-                std::cerr << "cd: " + path + ": No such file or directory" << std::endl;
+                std::cerr << "cd: " + path.at(0) + ": No such file or directory" << std::endl;
         }
         void echo(const std::vector<std::string>& args) {
             for (size_t i = 0; i < args.size(); ++i) {
