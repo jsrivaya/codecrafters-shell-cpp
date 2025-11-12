7b37c561379783add32542b9e4351f0961f4e5e1
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-12 03:44:14 -0800

    Remove singleton pattern from Command

--- src/command.cpp
+++ src/command.cpp
@@ -2,22 +2,14 @@
 #include "command_builtin.hpp"
 #include "command_custom.hpp"
 
-#include <mutex>
-#include <unordered_map>
+#include <memory>
 
 namespace shell {
 
     std::shared_ptr<Command> Command::get_command(const std::string& name) {
-        static std::unordered_map<std::string, std::shared_ptr<Command>> cache;
 
-        static std::mutex mtx;
-        std::lock_guard<std::mutex> lock(mtx);
         if (BuiltinCommand::is_builtin(name)) {
-            if(cache.contains(name)) {
-                return cache.at(name);
-            }
-            cache.emplace(name, std::make_shared<BuiltinCommand>(name));
-            return cache.at(name);
+            return std::make_shared<BuiltinCommand>(name);
         }
 
         return std::make_shared<CustomCommand>(name);
