3b65919c997ae360b4af84001eec7652ed7e38a5
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-20 07:26:44 -0800

    Add support for append cache to file

--- src/history.hpp
+++ src/history.hpp
@@ -7,7 +7,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <unordered_set>
+#include <ranges>
 namespace shell {
 
 class History {
@@ -35,6 +36,51 @@ public:
         }
 
         file.close();
+    }
+    void append_history_to_file(const std::string& history_file = "") {  
+        std::lock_guard<std::mutex> lock(mtx);
+        auto path = history_file.empty() ? get_path() : history_file;   
+
+        // load file in memory
+        std::deque<std::string> file_content{};
+        std::string line{};
+        std::ifstream file_read(path);
+        while (file_read.is_open() && std::getline(file_read, line)) {
+            // shell::Logger::getInstance().log("read from file: " + line);
+            file_content.emplace_back(line);
+        }
+
+        int itr_file = file_content.size() > 0 ?  file_content.size() - 1 : 0; // we start reading from the end of file
+        int itr_cache = 0; // we start reading from the begining of cache
+        int itr_to_append = 0;
+        // looking for the longest cache list that matches file history EOF
+        while(!file_content.empty() && itr_file >=0) {
+            if(cached_history.at(itr_cache) == file_content.at(itr_file)) {
+                auto itr_file_tmp = itr_file;
+                auto itr_cache_tmp = itr_cache;
+
+                while(itr_file_tmp != file_content.size() &&
+                    file_content.at(itr_file_tmp) == cached_history.at(itr_cache_tmp)) {
+                        ++itr_cache_tmp;
+                        ++itr_file_tmp;
+                }
+                if (itr_file_tmp == file_content.size()) {
+                    itr_to_append = itr_cache_tmp;
+                    break;
+                }
+                ++itr_cache;
+
+            } else {
+                --itr_file;
+            }
+        }
+
+        std::ofstream append_file(path, std::ios::app);
+        while (itr_to_append < cached_history.size()) {
+            append_file << cached_history.at(itr_to_append) << std::endl;
+            ++itr_to_append;
+        }
+        append_file.close();
     }
     void print_last(unsigned number = 0) {
         if (cached_history.empty()) {
@@ -69,7 +115,6 @@ protected:
     std::string get_path() {
         return persistent_path;
     }
-
 private:
     History() {
         if (const char* env = std::getenv("HISTFILE")) {
