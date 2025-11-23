0c314c0abff30780227131c92620f0b02c08c15f
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-23 04:35:15 -0800

    Clean up: extract parsing functionality to header

--- src/shell.hpp
+++ src/shell.hpp
@@ -1,91 +1,23 @@
 #pragma once
 
 #include "command.hpp"
-#include "command_line.hpp"
 #include "command_builtin.hpp"
+#include "command_line.hpp"
 #include "history.hpp"
 #include "logger.hpp"
+#include "shell_parser.hpp"
 #include "utils.hpp"
 
 #include <fcntl.h>
 #include <iostream>
 #include <sys/wait.h>
-#include <utility>
 #include <vector>
-#include <deque>
-
-#include <unistd.h>
 
 namespace shell{
 
 void init () {
     // initialize readline library
     rl_init();
-}
-// std::pair<std::string, std::vector<std::string>> get_args(const std::string& s) {
-// return: { arg0, arg1, arg2, arg3 }
-// (e.g {cat, /tmp/file.txt, >, myfile.txt})
-std::vector<std::string> get_tokens(const std::string& s) {
-    std::string current_token{};
-    std::vector<std::string> tokens{};
-    bool in_quotes = false;
-    bool in_double_quotes = false;
-    bool prev_space = false;
-    bool in_escaped = false;
-
-    for(const auto& c : s) {
-        if(in_escaped) {
-            // Inside double quotes: \ only escapes $, ', ", \, or newline — otherwise it's literal
-            if(in_double_quotes && (c !='$' && c != '`' && c != '"' && c != '\\')) {
-                current_token += "\\"; // we preseve backslash
-            }
-            current_token += c;
-            in_escaped = false;
-            continue;
-        }
-        if (in_quotes && c != '\'') {
-            current_token += c;
-            continue;
-        }
-        if (!in_quotes && c == '\\') {
-            // Outside quotes: \ preserves the literal value of the next character
-            in_escaped = !in_escaped;
-            continue;
-        }
-        if (!in_double_quotes && c == '\'') {
-            in_quotes = !in_quotes;
-            prev_space = false;
-            continue;
-        }
-        if (!in_quotes && c == '"') {
-            in_double_quotes = !in_double_quotes;
-            in_quotes = false;
-            prev_space = false;
-            continue;
-        }
-        if (in_double_quotes && c != '"') {
-            current_token += c;
-            continue;
-        }
-        if ((c == ' ' || c == '\t') && !in_quotes && !in_double_quotes) {
-            if(prev_space) continue;
-
-            prev_space = true;
-            tokens.emplace_back(current_token);
-            current_token = {};
-            continue;
-        }
-        current_token += c;
-        prev_space = false;
-    }
-    if(!current_token.empty()) tokens.emplace_back(current_token);
-
-    return tokens;
-}
-
-bool is_delimeter(const std::string& name) {
-    return name == "1>" || name == ">" || name == ">>" || name == "1>>" ||
-            name == "2>" || name == "2>>" || name == "|" || name == "&|";
 }
 
 std::vector<std::shared_ptr<Command>> get_commands(const std::string& command_line) {
